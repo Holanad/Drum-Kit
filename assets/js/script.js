8173bbc2102b5cb6b69c9980e2895d74f0944da7
Holanad <volkov99.at@gmail.com>
2021-08-18 21:18:42 +0300

    feat: add autoplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const notes = document.querySelectorAll('.note');
+const audio = document.querySelectorAll('audio');
+const btnAutoPlay = document.querySelector('.music-btn');
 
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -19,18 +21,38 @@ function mouseActive (){
     notes.forEach((i) => {
         i.addEventListener('click', function (){
            const btn = i.dataset.key;
-           const audios = document.querySelectorAll('audio');
-           console.log(audios)
-            /*audios.forEach((i) => {
-                i
-            });*/
+            audio.forEach((i) => {
+                const itemKey = i.dataset.key;
+                if(btn === itemKey){
+                    i.play();    
+                }
+            });
             i.classList.add('playing');
         });
     });  
-    
 }
 notes.forEach(note => note.addEventListener('transitionend', removeTransition));
 
+
+function autoPlay () {
+    btnAutoPlay.addEventListener('click', function (){
+        btnAutoPlay.classList.add('disable')
+        audio[0].play() && notes[0].classList.add('playing');
+        setTimeout(() => audio[1].play() && notes[1].classList.add('playing'), 1346);
+        setTimeout(() => audio[2].play() && notes[2].classList.add('playing'), 3490);
+        setTimeout(() => audio[3].play() && notes[3].classList.add('playing'), 4600);
+        setTimeout(() => audio[4].play() && notes[4].classList.add('playing'), 6100);
+        setTimeout(() => audio[5].play() && notes[5].classList.add('playing'), 7400);
+        setTimeout(() => audio[6].play() && notes[6].classList.add('playing'), 9200);
+        setTimeout(() => audio[7].play() && notes[7].classList.add('playing'), 10320);
+        setTimeout(() => audio[8].play() && notes[8].classList.add('playing'), 11800);
+        setTimeout(() => audio[9].play() && notes[9].classList.add('playing'), 13200);
+        setTimeout(() => btnAutoPlay.classList.remove('disable'), 13200);
+        
+    });
+}
+
+autoPlay ();
 mouseActive();
 keydownActive();
 
