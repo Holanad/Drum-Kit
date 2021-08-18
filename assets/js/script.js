f7d6b26c8c0d2e9a4f81d73b1e9e27c338ed9a50
Holanad <volkov99.at@gmail.com>
2021-08-18 16:49:51 +0300

    feat: add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const notes = document.querySelectorAll('.note');
+
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const note = document.querySelector(`.note[data-key="${e.keyCode}"]`);
@@ -9,9 +11,31 @@ function removeTransition(e){
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
-const notes = document.querySelectorAll('.note');
+
+function keydownActive (){
+    window.addEventListener('keydown', playSound);    
+}
+function mouseActive (){
+    notes.forEach((i) => {
+        i.addEventListener('click', function (){
+           const btn = i.dataset.key;
+           const audios = document.querySelectorAll('audio');
+           console.log(audios)
+            /*audios.forEach((i) => {
+                i
+            });*/
+            i.classList.add('playing');
+        });
+    });  
+    
+}
 notes.forEach(note => note.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+
+mouseActive();
+keydownActive();
+
+
+
 
 
 
